b04103e4830d6d6e37461c874aa73b3a40d3d9e0
Myloi Mellanc <mellanc@myloimellanc.local>
2018-01-10 17:12:11 +0900

    Initial Commit

---
+++ FlowerHive/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FlowerHive
+//
+//  Created by Myloi Mellanc on 2018. 1. 10..
+//  Copyright © 2018년 MyloiMellanc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
